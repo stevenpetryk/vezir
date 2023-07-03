a61967233f1cb355bac6e593ec5bdda8a7bfecc0
Steven Petryk <petryk.steven@gmail.com>
2023-07-03 11:31:16 -0700

    Harden algebraic notation check

--- src/engine/square.rs
+++ src/engine/square.rs
@@ -12,17 +12,19 @@ impl Square {
         let file_char = chars.get(0);
         let rank_char = chars.get(1);
 
-        let (file, rank) = match (file_char, rank_char) {
-            (Some(file_char), Some(rank_char)) => (
-                (*file_char as usize - 'a' as usize),
-                (*rank_char as usize) - ('1' as usize),
-            ),
-            _ => return Err(SquareParseError),
-        };
+        match (file_char, rank_char) {
+            (Some(file_char), Some(rank_char))
+                if ('a'..='h').contains(file_char) && ('1'..='8').contains(rank_char) =>
+            {
+                let file_index = (*file_char as usize) - ('a' as usize);
+                let rank_index = (*rank_char as usize) - ('1' as usize);
 
-        Ok(Square {
-            index: rank * 8 + file,
-        })
+                Ok(Square {
+                    index: rank_index * 8 + file_index,
+                })
+            }
+            _ => return Err(SquareParseError),
+        }
     }
 }
 
@@ -32,9 +34,19 @@ mod tests {
     use std::assert_matches::assert_matches;
 
     #[test]
-    fn from_algabraic_notation() {
+    fn from_algebraic_notation() {
         assert_matches!(Square::from_algebraic_notation("a1").unwrap().index, 0);
         assert_matches!(Square::from_algebraic_notation("b1").unwrap().index, 1);
         assert_matches!(Square::from_algebraic_notation("h8").unwrap().index, 63);
+
+        // Invalid
+        assert_matches!(
+            Square::from_algebraic_notation("z9"),
+            Err(SquareParseError {})
+        );
+        assert_matches!(
+            Square::from_algebraic_notation("asdf"),
+            Err(SquareParseError {})
+        );
     }
 }
